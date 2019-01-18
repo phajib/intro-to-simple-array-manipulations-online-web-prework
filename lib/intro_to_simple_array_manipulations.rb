a9106cd1e8c5d05eb2239eaa7c21eae0a9cd4ab4
H B <phajib@gmail.com>
2019-01-18 08:41:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,11 @@
+require 'benchmark'
+puts Benchmark.measure { 10_000_000.times { Object.new } }
+
 def using_push(array, element)
   array.push(element)
 end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+  
